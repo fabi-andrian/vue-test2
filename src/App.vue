34c2949011fce334b20992439e1fb4b377f63622
fabi-andrian <andrianirinafabrice2024@gmail.com>
2026-05-03 20:54:38 +0300

    list info person

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+<ul>
+  <li>{{ person.firstName }}</li>
+  <li>{{ person.lastName }}</li>
+  <li>{{ person.age }}</li>
+</ul>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+const person = ref ({
+  firstName: 'Michael',
+  lastName: 'Jackson',
+  age:  50,
+})
+</script>
